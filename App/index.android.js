83af6102a344b0c4d9a0a0a985edbff391c89771
jonperron <contact@jonathanperron.fr>
2017-01-07 15:46:18 +0100

    Updated App
    Nearly functionnal (but ugly...)

--- App/index.android.js
+++ App/index.android.js
@@ -16,29 +16,36 @@ import {
   View
 } from 'react-native';
 
+const onButtonPress = () => { Alert.alert('Button has been pressed!'); };
+
 class MyScannerApp extends Component {
   render() {
     return (
-      <View>
-        <Text>
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
           My Scanner
         </Text>
-		<HostIdentifier />
+        <HostIdentifier />
       </View>
     );
   }
 }
 
 class HostIdentifier extends Component {
+    constructor(props) {
+      super(props);
+      this.state= {hostname: "Hostname to look for",ip:"IP to scan"}
+    }
+	
 	render() {
 		return (
-			<View>
+			<View style={styles.identifier}>
 				<Text>Hostname: </Text>
-				<TextInput onChangeText={(hostname) => this.setState({hostname})} value={this.state.hostname}/>
-				<Button title="Identify"/>
+				<TextInput style={styles.textinput} onChangeText={(hostname) => this.setState({hostname})} value={this.state.hostname}/>
+                <Button onPress={onButtonPress} title="Resolve" color="#6495ed" accessibilityLabel="Identify this hostname"/>
 				<Text>IP: </Text>
-				<TextInput onChangeText={(ip) => this.setState({ip})} value={this.state.ip} />
-				<Scanner ip={this.state.ip} />
+				<TextInput style={styles.textinput} onChangeText={(ip) => this.setState({ip})} value={this.state.ip} />
+                <Scanner />
 			</View>
 		);
 	}
@@ -47,14 +54,14 @@ class HostIdentifier extends Component {
 class Scanner extends Component {
 		render() {
 			return (
-				<View>
+				<View style={styles.scanner}>
 					<Text>Scan</Text>
 					<Picker onValueChange={(port) => this.setState({port})}>
 						<Picker.Item label="FTP" value="21" />
 						<Picker.Item label="SSH" value="22" />
 						<Picker.Item label="HTTP" value="80" />
 					</Picker>
-					<Button title="Scan" />
+					<Button  onPress={onButtonPress} title="Scan" color="#6495ed" accessibilityLabel="Start scanner"/>
 					<ScanResults />
 				</View>
 			);
@@ -64,10 +71,46 @@ class Scanner extends Component {
 class ScanResults extends Component {
 	render() {
 		return(
-			<View>
+			<View style={styles.results}>
 				<Text> Scanning </Text>
 			</View>
 		);
 	}
 }
-AppRegistry.registerComponent('MyScannerApp', () => MyScannerApp);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'darkgreen',
+    fontFamily : 'Helvetice'
+  },
+  welcome: {
+    fontSize: 15,
+    justifyContent: 'center',
+    textAlign: 'center',
+    margin: 10,
+    color : "white",
+  },
+  identifier : {
+    flex:1,
+    margin:4,
+    justifyContent: 'center',
+    color : "white",
+  },
+  textinput: {
+    backgroundColor: 'white',
+    borderColor: 'black',
+    color : "black",
+  },
+  scanner : {
+    flex:15
+  },
+  results : {
+    flex:1,
+    backgroundColor: 'white',
+    borderColor: 'black',
+    color: "black",
+  }
+})
+
+AppRegistry.registerComponent('MyScannerApp', () => MyScannerApp)
